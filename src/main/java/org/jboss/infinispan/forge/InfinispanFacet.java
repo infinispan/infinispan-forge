31187b815cae30d9caee590b94baa027a99ff449
George Gastaldi <gegastaldi@gmail.com>
2012-08-31 12:04:55 -0300

    Initial commit

---
+++ src/main/java/org/jboss/infinispan/forge/InfinispanFacet.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.infinispan.forge;
+
+import org.jboss.forge.project.Facet;
+import org.jboss.forge.project.Project;
+
+public class InfinispanFacet implements Facet
+{
+   @Override
+   public Project getProject()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public void setProject(Project project)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public boolean install()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public boolean isInstalled()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public boolean uninstall()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+}
